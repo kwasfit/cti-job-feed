db54c451d49e7baea888ac563fbb4622cceb4e1a
Jen <inquiries@ctijen.com>
2025-06-22 07:28:58 -0400

    Fix credentials path for GitHub Actions

--- scrape-and-push.js
+++ scrape-and-push.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 puppeteer.use(StealthPlugin());
 
 // Load your service account credentials JSON file path here
-const credentials = JSON.parse(fs.readFileSync("/Users/jen-forregularuse/cti-job-scraper/cti-job-feed-3b4b4d4a2449.json"));
+const credentials = JSON.parse(fs.readFileSync("./cti-job-feed-3b4b4d4a2449.json"));
 
 const auth = new google.auth.JWT({
   email: credentials.client_email,
